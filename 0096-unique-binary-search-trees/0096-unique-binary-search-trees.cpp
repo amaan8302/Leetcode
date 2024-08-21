1cf51e4901810117aea0c6de928731c4b8f1b8aa
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-21 13:13:21 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (55.82%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
-    int numTrees(int n) 
+    int solve(int n, vector<int>&dp)
     {
         if(n==0||n==1)
             return 1;
+        if(dp[n]!=-1)
+            return dp[n];
         int ans = 0;
         for(int i = 1 ; i <=n ; i++)//ye saare root nodes banengi, ek ek karke
-            ans += numTrees(i-1)*numTrees(n-i);
-        return ans;
+            ans += solve(i-1,dp)*solve(n-i,dp);
+        dp[n]=ans;
+        return dp[n];
+    }
+    int numTrees(int n) 
+    {
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
     }
 };
