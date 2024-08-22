93e4f7d354d85d532c92a1b85e19a31792f7921a
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-22 20:33:18 +0530

    Time: 35 ms (38.91%), Space: 14.9 MB (10.79%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,32 +1,5 @@
 class Solution {
-public:
-    int solve(vector<int>& arr,map<pair<int,int>,int>&maxi,int left,int right,vector<vector<int>>&dp)
-    {
-        if(left==right)//leaf node he
-            return 0;
-        if(dp[left][right]!=-1)
-            return dp[left][right];
-        int ans = INT_MAX;
-        for(int i = left;i<right;i++)
-             ans = min(ans,maxi[{left,i}]*maxi[{i+1,right}]+solve(arr,maxi,left,i,dp)+solve(arr,maxi,i+1,right,dp));
-        return dp[left][right]=ans;
-    }
-    // int solvetab(vector<int>& arr,map<pair<int,int>,int>&maxi)
-    // {
-    //     int n = arr.size();
-    //     vector<vector<int>>dp(arr.size()+2,vector<int>(arr.size()+2,0));
-    //     for(int left = n-1 ; left>=0 ; left--)
-    //     {
-    //         for(int right = 0 ; right < n ; right++)
-    //         {
-    //             int ans = INT_MAX;
-    //             for(int i = left;i<right;i++)
-    //                  ans = min(ans,maxi[{left,i}]*maxi[{i+1,right}]+dp[left][i]+dp[i+1][right]);
-    //             dp[left][right]=ans;
-    //         }
-    //     }
-    //     return dp[0][n-1];
-    // }
+public:     
     int solvetab(vector<int>& arr, map<pair<int,int>,int>& maxi)
     {
         int n = arr.size();
