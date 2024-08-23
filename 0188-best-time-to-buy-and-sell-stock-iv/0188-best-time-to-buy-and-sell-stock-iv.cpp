0b865c077a88dfe2c4441ef2b9d88fba61c0197a
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-23 17:36:49 +0530

    Time: 7 ms (77.21%), Space: 13.4 MB (82.90%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
-        // vector<vector<int>>curr(2,vector<int>(k+1,0))''
+        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
+        vector<vector<int>>curr(2,vector<int>(k+1,0));
+        vector<vector<int>>next(2,vector<int>(k+1,0));
         for(int index = n-1 ; index>=0 ; index--)
         {
             for(int buy = 0 ; buy <=1 ; buy++)
@@ -13,20 +14,21 @@ public:
                     int profit;
                     if(buy)
                     {
-                        int buy = -prices[index]+dp[index+1][0][limit];
-                        int ignore = dp[index+1][1][limit];
+                        int buy = -prices[index]+next[0][limit];
+                        int ignore = next[1][limit];
                         profit = max(buy,ignore); 
                     }
                     else
                     {
-                        int sell = dp[index+1][1][limit-1]+prices[index];
-                        int ignore = dp[index+1][0][limit];
+                        int sell = next[1][limit-1]+prices[index];
+                        int ignore = next[0][limit];
                         profit=max(sell,ignore);
                     }
-                    dp[index][buy][limit]=profit;   
+                    curr[buy][limit]=profit;   
                 }
+                next=curr;
             }
         }
-        return dp[0][1][k];
+        return next[1][k];
     }
 };
