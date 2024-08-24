afc426399d0112ef4fea48110f9cb00edac2cc9d
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-24 20:54:56 +0530

    Time: 4 ms (88.71%), Space: 16.3 MB (57.60%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,31 +1,9 @@
 class Solution {
 public:
-    int solve(int index,int transactionNum,int k, vector<int>& prices,vector<vector<int>>&dp)
-    {
-        if(index==prices.size()||transactionNum>=2*k)
-            return 0;
-        if(dp[index][transactionNum]!=-1)
-            return dp[index][transactionNum];
-        int profit;
-        
-        if(transactionNum%2==0)//buy karna allowed he
-        {
-            int buy = -prices[index]+solve(index+1,transactionNum+1,k,prices,dp);
-            int ignore = solve(index+1,transactionNum,k,prices,dp);
-            profit = max(buy,ignore); 
-        }
-        else
-        {
-            int sell = solve(index+1,transactionNum+1,k,prices,dp)+prices[index];
-            int ignore = solve(index+1,transactionNum,k,prices,dp);
-            profit=max(sell,ignore);
-        }
-        return dp[index][transactionNum]=profit;
-    }
     int maxProfit(int k, vector<int>& prices)
     {
         int n = prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2*k,0));
+        vector<vector<int>>dp(n+1,vector<int>(2*k+1,0));
         for(int index=n-1;index>=0;index--)
         {
             for(int transactionNum = 2*k-1 ; transactionNum>=0 ; transactionNum--)
@@ -34,14 +12,14 @@ public:
         
                 if(transactionNum%2==0)//buy karna allowed he
                 {
-                    int buy = -prices[index]+solve(index+1,transactionNum+1,k,prices,dp);
-                    int ignore = solve(index+1,transactionNum,k,prices,dp);
+                    int buy = -prices[index]+dp[index+1][transactionNum+1];
+                    int ignore = dp[index+1][transactionNum];
                     profit = max(buy,ignore); 
                 }
                 else
                 {
-                    int sell = solve(index+1,transactionNum+1,k,prices,dp)+prices[index];
-                    int ignore = solve(index+1,transactionNum,k,prices,dp);
+                    int sell = dp[index+1][transactionNum+1]+prices[index];
+                    int ignore = dp[index+1][transactionNum];
                     profit=max(sell,ignore);
                 }
                 dp[index][transactionNum]=profit;
@@ -49,10 +27,11 @@ public:
         }
         return dp[0][0];
     }
-    // int maxProfit(int k, vector<int>& prices)
-    // {
-    //     int n = prices.size();
-    //     vector<vector<int>>dp(n+1,vector<int>(2*k,-1));
-    //     return solve(0,0,k,prices,dp);    
-    // }
+//     int maxProfit(int k, vector<int>& prices)
+//     {
+//         int n = prices.size();
+//         vector<int>curr(2*k,0);
+//         vector<int>curr(2*k,0);
+        
+//     }
 };
