527088bfc2b29156f769a82ac587ef010a2d848b
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-24 23:25:53 +0530

    Time: 81 ms (73.47%), Space: 57.7 MB (84.08%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,31 +1,5 @@
 class Solution {
 public:
-    // int maxProfit(vector<int>& prices, int fee) 
-    // {
-    //     int n = prices.size();
-    //     vector<vector<int>>dp(n+1,vector<int>(2,0));
-    //     for(int index = n-1 ;index>=0;index--)
-    //     {
-    //         for(int buy = 0 ; buy <= 1 ; buy++)
-    //         {
-    //             int profit;
-    //             if(buy)
-    //             {
-    //                 int buyKaro = dp[index+1][0]-prices[index];
-    //                 int ignore = dp[index+1][1];
-    //                 profit = max(ignore,buyKaro);
-    //             }
-    //             else
-    //             {
-    //                 int sell = dp[index+1][1]+prices[index]-fee;
-    //                 int ignore = dp[index+1][0];
-    //                 profit = max(sell,ignore);
-    //             }
-    //             dp[index][buy]=profit;
-    //         }
-    //     }
-    //     return dp[0][1];
-    // }
     int maxProfit(vector<int>& prices, int fee) 
     {
         int n = prices.size();
@@ -38,13 +12,13 @@ public:
                 int profit;
                 if(buy)
                 {
-                    int buyKaro = next[0]-prices[index];
+                    int buyKaro = next[0]-prices[index]-fee;
                     int ignore = next[1];
                     profit = max(ignore,buyKaro);
                 }
                 else
                 {
-                    int sell = next[1]+prices[index]-fee;
+                    int sell = next[1]+prices[index];
                     int ignore = next[0];
                     profit = max(sell,ignore);
                 }
