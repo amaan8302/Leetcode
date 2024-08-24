7a6e5cc1bd71b00d364ce22f31c36697f38f1b91
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-24 23:21:27 +0530

    Time: 73 ms (83.69%), Space: 57.7 MB (76.00%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,9 +1,36 @@
 class Solution {
 public:
+    // int maxProfit(vector<int>& prices, int fee) 
+    // {
+    //     int n = prices.size();
+    //     vector<vector<int>>dp(n+1,vector<int>(2,0));
+    //     for(int index = n-1 ;index>=0;index--)
+    //     {
+    //         for(int buy = 0 ; buy <= 1 ; buy++)
+    //         {
+    //             int profit;
+    //             if(buy)
+    //             {
+    //                 int buyKaro = dp[index+1][0]-prices[index];
+    //                 int ignore = dp[index+1][1];
+    //                 profit = max(ignore,buyKaro);
+    //             }
+    //             else
+    //             {
+    //                 int sell = dp[index+1][1]+prices[index]-fee;
+    //                 int ignore = dp[index+1][0];
+    //                 profit = max(sell,ignore);
+    //             }
+    //             dp[index][buy]=profit;
+    //         }
+    //     }
+    //     return dp[0][1];
+    // }
     int maxProfit(vector<int>& prices, int fee) 
     {
         int n = prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        vector<int>curr(2,0);
+        vector<int>next(2,0);
         for(int index = n-1 ;index>=0;index--)
         {
             for(int buy = 0 ; buy <= 1 ; buy++)
@@ -11,19 +38,20 @@ public:
                 int profit;
                 if(buy)
                 {
-                    int buyKaro = dp[index+1][0]-prices[index];
-                    int ignore = dp[index+1][1];
+                    int buyKaro = next[0]-prices[index];
+                    int ignore = next[1];
                     profit = max(ignore,buyKaro);
                 }
                 else
                 {
-                    int sell = dp[index+1][1]+prices[index]-fee;
-                    int ignore = dp[index+1][0];
+                    int sell = next[1]+prices[index]-fee;
+                    int ignore = next[0];
                     profit = max(sell,ignore);
                 }
-                dp[index][buy]=profit;
+                curr[buy]=profit;
             }
+            next=curr;
         }
-        return dp[0][1];
+        return next[1];
     }
 };
