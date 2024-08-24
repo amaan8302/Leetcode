780963dd4fe2f3ddf795eb500c9eccfb152a261f
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-24 20:56:50 +0530

    Time: 4 ms (88.71%), Space: 13.4 MB (86.15%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -3,7 +3,8 @@ public:
     int maxProfit(int k, vector<int>& prices)
     {
         int n = prices.size();
-        vector<vector<int>>dp(n+1,vector<int>(2*k+1,0));
+        vector<int>curr(2*k+1,0);
+        vector<int>next(2*k+1,0);
         for(int index=n-1;index>=0;index--)
         {
             for(int transactionNum = 2*k-1 ; transactionNum>=0 ; transactionNum--)
@@ -12,26 +13,20 @@ public:
         
                 if(transactionNum%2==0)//buy karna allowed he
                 {
-                    int buy = -prices[index]+dp[index+1][transactionNum+1];
-                    int ignore = dp[index+1][transactionNum];
+                    int buy = -prices[index]+next[transactionNum+1];
+                    int ignore =next[transactionNum];
                     profit = max(buy,ignore); 
                 }
                 else
                 {
-                    int sell = dp[index+1][transactionNum+1]+prices[index];
-                    int ignore = dp[index+1][transactionNum];
+                    int sell = next[transactionNum+1]+prices[index];
+                    int ignore = next[transactionNum];
                     profit=max(sell,ignore);
                 }
-                dp[index][transactionNum]=profit;
+                curr[transactionNum]=profit;
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
-//     int maxProfit(int k, vector<int>& prices)
-//     {
-//         int n = prices.size();
-//         vector<int>curr(2*k,0);
-//         vector<int>curr(2*k,0);
-        
-//     }
 };
