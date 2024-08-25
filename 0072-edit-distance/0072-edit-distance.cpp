b551952e75d2215855d75a1680ef83ffd6d54dec
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-25 13:59:48 +0530

    Time: 9 ms (50.37%), Space: 13.2 MB (24.94%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    int solve(string &a, string &b , int i , int j,vector<vector<int>>&dp)
-    {
-       if(i==a.length())
-           return b.length()-j;
-        if(j==b.length())
-            return a.length()-i;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int ans = INT_MAX;
-        if(a[i]==b[j])
-            ans = solve(a,b,i+1,j+1,dp);
-        else
-        {
-            int insertion,deletion,replace;
-            //insert
-            insertion = 1 + solve(a,b,i,j+1,dp);
-            //deletion
-            deletion = 1 + solve(a,b,i+1,j,dp);
-            //replacement
-            replace = 1 + solve(a,b,i+1,j+1,dp);
-            ans = min(insertion,min(deletion, replace));
-        }
-        return dp[i][j]=ans;
-    }
     int minDistance(string word1, string word2) {
-        
-            vector<vector<int>>dp(word1.length()+1,vector<int>(word2.length()+1,-1));
-            return solve(word1,word2,0,0,dp);
+        vector<vector<int>>dp(word1.length()+2,vector<int>(word2.length()+2,0));
+        for(int i = 0 ; i < word1.length();i++)
+            dp[i][word2.length()]=word1.length()-i;
+        for(int i = 0 ; i < word2.length();i++)
+            dp[word1.length()][i]=word2.length()-i;
+        for(int i = word1.length()-1; i>=0 ;i--)
+        {
+            for(int j = word2.length()-1 ; j>=0 ; j--)
+            {
+                if(word1[i]==word2[j])
+                    dp[i][j] = dp[i+1][j+1];
+                else
+                {
+                    int insertion,deletion,replace;
+                    //insert
+                    insertion = 1 + dp[i][j+1];
+                    //deletion
+                    deletion = 1 + dp[i+1][j];
+                    //replacement
+                    replace = 1 + dp[i+1][j+1];
+                    dp[i][j] = min(insertion,min(deletion, replace));
+                }
+            }
+        }
+        return dp[0][0];
+        // return solve(word1,word2,0,0,dp);
     }
 };
