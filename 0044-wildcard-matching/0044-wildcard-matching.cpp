0b6e046e43c130176cbacc4e866d13ef2bd1015f
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-26 12:19:13 +0530

    Time: 35 ms (61.18%), Space: 31.8 MB (14.66%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     bool solve(string &s, string &p , int i , int j,vector<vector<int>>&dp)
     {
-        if(i<0 && j<0) //dono strings poori consume ho gai he, return true
+        if(i==0 && j==0) //dono strings poori consume ho gai he, return true
             return 1;
-        if(i>=0 && j<0)//str not consumed but p is consumed, invalid
+        if(i>=0 && j==0)//str not consumed but p is consumed, invalid
             return 0;
-        if(i<0 && j>=0)
+        if(i==0 && j>=0)
         {
-            for(int i = 0 ; i <= j ; i++)
+            for(int i = 1 ; i <= j ; i++)
             {
-                if(p[i]!='*')
+                if(p[i-1]!='*')
                     return 0;
             }
             return 1;
@@ -18,9 +18,9 @@ public:
         if(dp[i][j]!=-1)
             return dp[i][j];
         bool ans = 0;
-        if(s[i]==p[j]||p[j]=='?')
+        if(s[i-1]==p[j-1]||p[j-1]=='?')
             ans= solve(s,p,i-1,j-1,dp);
-        else if(p[j]=='*')
+        else if(p[j-1]=='*')
             ans = (solve(s,p,i-1,j,dp) or solve(s,p,i,j-1,dp));
         else
             return 0;
@@ -29,6 +29,8 @@ public:
     }
     bool isMatch(string s, string p) {
         vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
-        return solve(s,p,s.size()-1,p.size()-1,dp);
+        return solve(s,p,s.size(),p.size(),dp);
     }
 };
+
+
