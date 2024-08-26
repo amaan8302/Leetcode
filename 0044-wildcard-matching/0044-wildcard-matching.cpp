7ac274beec31f139658749d4a104deb7b1b87181
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-26 12:31:38 +0530

    Time: 69 ms (26.87%), Space: 16.5 MB (70.79%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
-    bool solve(string &s, string &p , int i , int j,vector<vector<int>>&dp)
-    {
-        if(i==0 && j==0) //dono strings poori consume ho gai he, return true
-            return 1;
-        if(i>=0 && j==0)//str not consumed but p is consumed, invalid
-            return 0;
-        if(i==0 && j>=0)
-        {
-            for(int i = 1 ; i <= j ; i++)
-            {
-                if(p[i-1]!='*')
-                    return 0;
-            }
-            return 1;
-        }
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        bool ans = 0;
-        if(s[i-1]==p[j-1]||p[j-1]=='?')
-            ans= solve(s,p,i-1,j-1,dp);
-        else if(p[j-1]=='*')
-            ans = (solve(s,p,i-1,j,dp) or solve(s,p,i,j-1,dp));
-        else
-            return 0;
-        return dp[i][j]=ans;
-            
-    }
     bool isMatch(string s, string p) {
-        vector<vector<int>>dp(s.size()+1,vector<int>(p.size()+1,-1));
-        return solve(s,p,s.size(),p.size(),dp);
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
+
+        // Initialize dp for the case where s is empty
+        for (int j = 1; j <= n; j++) {
+            if (p[j - 1] == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
-
-
