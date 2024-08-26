d183a2307fa5573e9d70999fbbeb61334301ebde
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-26 21:28:51 +0530

    Time: 5 ms (82.14%), Space: 33.3 MB (16.27%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int expandAroundCenter(const string &s, int left, int right) 
+    {
+        int count = 0;
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            ++count;
+            --left;
+            ++right;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int n = s.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        int count = 0;
+        for (int i = 0; i < n; ++i) {
+            count += expandAroundCenter(s, i, i);
+            count += expandAroundCenter(s, i, i + 1);
+        }
+        return dp[n][n]=count;
+    }
+};
