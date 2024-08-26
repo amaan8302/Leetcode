f2c171363fe89bccf0d02a581582031604648fba
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-26 12:42:18 +0530

    Time: 20 ms (91.50%), Space: 10.9 MB (92.99%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
+    bool isMatch(string s, string p)
+    {
         int m = s.size(), n = p.size();
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-        dp[0][0] = true;
-
-        // Initialize dp for the case where s is empty
-        for (int j = 1; j <= n; j++) {
-            if (p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 1];
-            }
+        vector<int>curr(n+1,0),prev(n+1,0);
+        prev[0]=1;
+        for (int j = 1; j <= n; j++) 
+        {
+            if (p[j - 1] == '*') 
+                prev[j] = prev[j - 1];
         }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p[j - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                } else {
-                    dp[i][j] = false;
-                }
+        for (int i = 1; i <= m; i++) 
+        {
+            for (int j = 1; j <= n; j++) 
+            {
+                if (p[j - 1] == s[i - 1] || p[j - 1] == '?') 
+                    curr[j] = prev[j - 1];
+                else if (p[j - 1] == '*') 
+                    curr[j] = prev[j] || curr[j - 1];
+                else 
+                    curr[j] = false;
             }
+            prev=curr;
         }
-        return dp[m][n];
+        return prev[n];
     }
 };
