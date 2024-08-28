22748e56966fe16e34c3b6577691599762cc6ab0
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-29 01:06:16 +0530

    Time: 970 ms (5.17%), Space: 10.9 MB (88.38%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,12 +1,35 @@
 class Solution {
 public:
+    // int solve(vector<int>& nums, int target,int index,vector<vector<int>>&dp)
+    // {
+    //     if(target == 0 && index == nums.size())
+    //         return 1;
+    //     if(index>=nums.size())
+    //         return 0;
+    //     if(dp[index][target]!=-1)
+    //         return dp[index][target];
+    //     int plus;
+    //     plus = solve(nums,target-nums[index],index+1,dp);
+    //     int minus = solve(nums,target+nums[index],index+1,dp);
+    //     return dp[index][target]=plus+minus;
+    // }
+    // int findTargetSumWays(vector<int>& nums, int target) 
+    // {
+    //     int n = nums.size();
+    //     int sum = 0;
+    //     for(int i : nums)
+    //         sum+=i;
+    //     vector<vector<int>>dp(n+1,vector<int>(target+sum+1,-1));
+    //     return solve(nums,target,0,dp);
+    // }
     int solve(vector<int>& nums, int target,int index)
     {
         if(target == 0 && index == nums.size())
             return 1;
         if(index>=nums.size())
             return 0;
-        int plus = solve(nums,target-nums[index],index+1);
+        int plus;
+        plus = solve(nums,target-nums[index],index+1);
         int minus = solve(nums,target+nums[index],index+1);
         return plus+minus;
     }
