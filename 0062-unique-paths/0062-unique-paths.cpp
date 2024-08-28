61d2c66bcb8c61a882e0a154814f99c62123d8da
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-08-28 14:37:30 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (49.64%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    const int mod = 2000000000;
+
+    int solve(int m, int n, vector<vector<int>>& dp, int i, int j) {
+        if (i == 0 || j == 0) 
+            return 1;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        dp[i][j] = (solve(m, n, dp, i - 1, j) + solve(m, n, dp, i, j - 1)) % mod;
+
+        return dp[i][j];
+    }
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m, n, dp, m - 1, n - 1);
+    }
+};
