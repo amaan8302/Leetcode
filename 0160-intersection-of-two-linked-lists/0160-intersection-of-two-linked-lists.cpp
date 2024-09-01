c9ad0319194057c0932e5f9489290d92746e503b
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-09-01 19:19:26 +0530

    Time: 38 ms (52.92%), Space: 17.4 MB (24.82%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,27 +1,43 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        
-        unordered_map<ListNode *,bool>visited;
-        ListNode *temp1=headA;
-        ListNode *temp2=headB;
-        while(temp1!=NULL||temp2!=NULL)
+        int n1 = 0, n2 = 0; 
+        ListNode *temp1 = headA, *temp2 = headB;
+
+        while (temp1 != NULL) 
         {
-            if(temp1!=NULL)
-            {
-                if(visited[temp1])
-                    return temp1;
-                visited[temp1]=1;
-                temp1=temp1->next;
-            }
-            if(temp2!=NULL)
-            {
-                if(visited[temp2])
-                    return temp2;
-                visited[temp2]=1;
-                temp2=temp2->next;
-            }
+            n1++;
+            temp1 = temp1->next;
         }
+
+        while (temp2 != NULL) 
+        {
+            n2++;
+            temp2 = temp2->next;
+        }
+
+        temp1 = headA;
+        temp2 = headB;
+
+        if (n1 > n2)
+        {
+            int diff = n1 - n2;
+            while (diff--) 
+                temp1 = temp1->next;
+        } 
+        else if (n2 > n1) 
+        {
+            int diff = n2 - n1;
+            while (diff--) 
+                temp2 = temp2->next;
+        }
+
+        while (temp1 != NULL && temp2 != NULL) {
+            if (temp1 == temp2) return temp1;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+
         return NULL;
     }
 };
