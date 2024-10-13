44940b0c9ceb2d9e2b3d0a9ff2684888ebc15cb5
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-10-13 12:56:47 +0530

    Time: 36 ms (48.23%), Space: 12.6 MB (18.93%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int solve(int left, int right, vector<int>& cuts, vector<vector<int>>& dp) {
-        if (right - left < 2)
+    int solve(int n, vector<int>& cuts, int left, int right,vector<vector<int>>&dp)
+    {
+        if(right-left<2)
             return 0;
-        if (dp[left][right] != -1)
+        if(dp[left][right]!=-1)
             return dp[left][right];
-        int result = INT_MAX;
-        for (int i = left + 1; i < right; i++) {
-            int cost = cuts[right] - cuts[left] + solve(left, i, cuts, dp) + solve(i, right, cuts, dp);
-            result = min(result, cost);
+        int result = INT_MAX, cost;
+        for(int i = left+1 ; i < right ; i++)
+        {
+            cost = cuts[right]-cuts[left] + solve(n,cuts,left,i,dp) + solve(n,cuts,i,right,dp);
+            result = min(result,cost);
         }
-        return dp[left][right] = result;
+        return dp[left][right]=result;
     }
-    
-    int minCost(int n, vector<int>& cuts) {
-        int m = cuts.size();
+    int minCost(int n, vector<int>& cuts) 
+    {
+        
+        std::sort(cuts.begin(),cuts.end());
         cuts.insert(cuts.begin(), 0);
         cuts.push_back(n);
-        vector<vector<int>> dp(m + 2, vector<int>(m + 2, -1));
-        std::sort(cuts.begin(), cuts.end());
-        return solve(0, m + 1, cuts, dp);
+        int m = cuts.size();
+        vector<vector<int>>dp(m+1,vector<int>(m+1,-1));
+        return solve(n,cuts,0,m-1,dp);
     }
 };
