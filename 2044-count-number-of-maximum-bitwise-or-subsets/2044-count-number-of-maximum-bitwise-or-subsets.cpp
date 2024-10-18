1a78507cbfe51e5ce9c673e86822639ed2e8e26e
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-10-18 18:13:57 +0530

    Time: 248 ms (10.86%), Space: 305.9 MB (10.46%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int solve(int i , int curr, vector<int>& nums, int n,vector<vector<int>>&dp)
-    {
-        if(i==nums.size())
-        {
-            if(curr==n)
-                return 1;
-            return 0;
-        }
-        if(dp[i][curr]!=-1)
-            return dp[i][curr];
-        int take = solve(i+1,curr|nums[i],nums,n,dp);
-        int no = solve(i+1,curr,nums,n,dp);
-        return dp[i][curr]=take+no;
-    }
-    int countMaxOrSubsets(vector<int>& nums) 
-    {
+    int countMaxOrSubsets(vector<int>& nums) {
         int n = 0;
-        for(int &i : nums)
-            n |=i;
-        vector<vector<int>>dp(nums.size()+1,vector<int>(n+1,-1));
-        int curr = 0;
-        return solve(0,curr,nums,n,dp);
+        for (int &i : nums) {
+            n |= i;
+        }
+        int size = nums.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int num : nums)
+        {
+            vector<int> temp = dp;
+            for (int i = 0; i <= n; ++i)
+            {
+                if (dp[i]) 
+                    temp[i | num] += dp[i];
+            }
+            dp = temp;
+        }
+        return dp[n];
     }
 };
