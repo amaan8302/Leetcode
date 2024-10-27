b0d03ceb82c6e19da6067359f6238499ade7e7d9
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-10-27 13:20:20 +0530

    Time: 7 ms (51.85%), Space: 29.2 MB (27.15%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
-    int solve(int i , int j , vector<vector<int>>& matrix,vector<vector<int>>&dp)
+    // int solve(int i , int j , vector<vector<int>>& matrix,vector<vector<int>>&dp)
+    // {
+    //     int m = matrix.size();
+    //     int n = matrix[0].size();
+    //     if(i<0 || i>=m || j<0 || j>=n ||matrix[i][j]!=1)
+    //         return 0;
+    //     if(dp[i][j]!=-1)
+    //         return dp[i][j];
+    //     int right = solve(i,j+1,matrix,dp);
+    //     int down = solve(i+1,j,matrix,dp);
+    //     int diagonal = solve(i+1,j+1,matrix,dp);
+    //     return dp[i][j] = 1 + min({right,down,diagonal});
+    // }
+    // int countSquares(vector<vector<int>>& matrix) {
+    //     int m = matrix.size();
+    //     int n = matrix[0].size();
+    //     int cnt = 0;
+    //     vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    //     for(int i = 0 ; i < m ; i++)
+    //     {
+    //         for(int j = 0 ; j < n ; j++)
+    //         {
+    //             if(matrix[i][j]==1)
+    //                 cnt+= solve(i,j,matrix,dp);
+    //         }
+    //     }
+    //     return cnt;
+    // }
+    int countSquares(vector<vector<int>>& matrix)
     {
         int m = matrix.size();
         int n = matrix[0].size();
-        if(i<0 || i>=m || j<0 || j>=n ||matrix[i][j]!=1)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int right = solve(i,j+1,matrix,dp);
-        int down = solve(i+1,j,matrix,dp);
-        int diagonal = solve(i+1,j+1,matrix,dp);
-        return dp[i][j] = 1 + min({right,down,diagonal});
-    }
-    int countSquares(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
         int cnt = 0;
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        for(int i = 0 ; i < m ; i++)
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for (int i = 1; i <= m; i++)
         {
-            for(int j = 0 ; j < n ; j++)
+            for (int j = 1; j <= n; j++)
             {
-                if(matrix[i][j]==1)
-                    cnt+= solve(i,j,matrix,dp);
+                if (matrix[i-1][j-1] == 1)
+                {
+                    int down = dp[i-1][j];
+                    int right = dp[i][j-1];
+                    int diagonal = dp[i-1][j-1];
+                    dp[i][j] = 1 + min({right, down ,diagonal});
+                    cnt += dp[i][j];
+                }
             }
         }
         return cnt;
     }
+
 };
