542ff12e1e147927cc82b6d07cb86451d9f46423
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-11-15 18:03:58 +0530

    Time: 23 ms (5.80%), Space: 108 MB (5.04%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    vector<int> dp;
-    
-    int solve(int i, vector<int>& prices, int mini) {
+
+    int solve(int i, vector<int>& prices, int mini,vector<int>&dp) {
         if (i == prices.size()) return 0;
         if (dp[i] != -1) return dp[i];
-
-        // Compute the profit for the current day and the next recursive call
-        int profit = max(solve(i + 1, prices, min(mini, prices[i])), prices[i] - mini);
-        
-        // Memoize the result
+        int profit = max(solve(i + 1, prices, min(mini, prices[i]),dp), prices[i] - mini);
         dp[i] = profit;
         return dp[i];
     }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        dp.assign(n, -1);
-        return solve(0, prices, prices[0]);
+        vector<int>dp(n+1,-1);
+        return solve(0, prices, prices[0],dp);
     }
 };
