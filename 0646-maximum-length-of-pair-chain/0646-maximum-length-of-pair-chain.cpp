9b5975e7eef92820b1bac89d8185f8ba066f04ab
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-12-07 23:27:37 +0530

    Time: 302 ms (9.82%), Space: 163.5 MB (6.37%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,24 +1,43 @@
+// class Solution {
+// public:
+//     int solve(vector<vector<int>>& p , int n , int curr , int prev , vector<vector<int>>&dp)
+//     {
+//         if(curr>=n)
+//             return 0;
+//         if(dp[curr][prev+1]!=-1)
+//             return dp[curr][prev];
+//         int incl = 0;
+//         if(prev==-1||p[curr][0]>p[prev][1])
+//             incl = 1 + solve(p,n,curr+1,curr,dp);
+//         int excl = solve(p,n,curr+1,prev,dp);
+//         return dp[curr][prev+1] = max(incl,excl);
+//     }
+//     int findLongestChain(vector<vector<int>>& pairs) 
+//     {
+//         std::sort(pairs.begin(),pairs.end());
+//         int n = pairs.size();
+//         vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+//         return solve(pairs,n,0,-1,dp);
+//     }
+// };
 class Solution {
 public:
     int solve(vector<vector<int>>& p, int n, int curr, int prev, vector<vector<int>>& dp) {
         if (curr >= n)
             return 0;
-        if (prev != -1 && dp[curr][prev] != -1)
-            return dp[curr][prev];
+        if (dp[curr][prev + 1] != -1)
+            return dp[curr][prev + 1];
         int incl = 0;
         if (prev == -1 || p[curr][0] > p[prev][1])
             incl = 1 + solve(p, n, curr + 1, curr, dp);
         int excl = solve(p, n, curr + 1, prev, dp);
-        if (prev != -1)
-            dp[curr][prev] = max(incl, excl);
-        
-        return max(incl, excl);
+        return dp[curr][prev + 1] = max(incl, excl);
     }
-    
+
     int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(), pairs.end());
+        std::sort(pairs.begin(), pairs.end());
         int n = pairs.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
         return solve(pairs, n, 0, -1, dp);
     }
 };
