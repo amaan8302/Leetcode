441a48281be16e553ba806d5556ed355e950dfbe
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2024-12-11 12:43:36 +0530

    Time: 23 ms (5.80%), Space: 84.3 MB (41.67%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -10,26 +10,25 @@ public:
         int maxi = INT_MIN;
         int i = 0, j = 0;
         int n = nums.size();
-        vector<int>res(2*n);
-        for(int i = 0 ; i < 2*n ; i++)
-            res[i] = nums[(n+i)%n];
+        // vector<int>res(2*n);
+        // for(int i = 0 ; i < 2*n ; i++)
+        //     res[i] = nums[(n+i)%n];
         int CurrOnes = 0;
         while(j<2*n)
         {
             if(j - i + 1<=size)
             {
-                if(res[j]==1)
+                if(nums[j%n]==1)
                     CurrOnes++;
                 j++;
             }
             else
             {
                 maxi = max(maxi , CurrOnes);
-                if(res[i]==1)
+                if(nums[i%n]==1)
                     CurrOnes--;
                 i++;
             }
-            // j++;
         }
         return size - maxi;
     }
