8c0aa399479fd66ce46166be1241dff80c2b277d
Amaan Ullah Siddiqui <114748568+amaan8302@users.noreply.github.com>
2025-02-10 19:18:07 +0530

    Time: 2 ms, Memory: 8.3 MB - LeetHub

--- 0403-15-3174-clear-digits/0403-15-3174-clear-digits.cpp
+++ 0403-15-3174-clear-digits/0403-15-3174-clear-digits.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     string clearDigits(string s) 
     {
-        for (int i = 0; i < s.length(); ) {
-        if (isdigit(s[i])) {
-            s.erase(i, 1);
-            if (i > 0) {
-                s.erase(i - 1, 1);
-                i--;
-            }
-        } else {
-            i++;
+        for (int i = 0; i < s.length(); ) 
+        {
+            if (isdigit(s[i])) 
+            {
+                s.erase(i, 1);
+                if (i > 0) 
+                {
+                    s.erase(i - 1, 1);
+                    i--;
+                }
+            } 
+            else
+                i++;
         }
-    }
         return s;
     }
 };
